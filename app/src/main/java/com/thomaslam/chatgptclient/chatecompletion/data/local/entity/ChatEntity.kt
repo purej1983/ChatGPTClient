247ca47f5f5a29ac3aa1941ef851dc7d410b440e
Thomas Lam <purej1983@gmail.com>
2023-05-21 09:58:22 +0100

    feat: import room db

---
+++ app/src/main/java/com/thomaslam/chatgptclient/chatecompletion/data/local/entity/ChatEntity.kt
@@ -0,0 +1,20 @@
+package com.thomaslam.chatgptclient.chatecompletion.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.thomaslam.chatgptclient.chatecompletion.domain.entity.Chat
+
+@Entity(
+    tableName = "Chat"
+)
+data class ChatEntity(
+    val lastUserMessage: String = "New Chat",
+    @PrimaryKey val id: Long? = null
+) {
+    fun toChat(): Chat {
+        return Chat(
+            lastUserMessage = lastUserMessage,
+            id = id
+        )
+    }
+}
