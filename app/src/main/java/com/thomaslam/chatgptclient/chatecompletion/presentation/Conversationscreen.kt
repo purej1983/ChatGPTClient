dd811cd0df073a6b7f46824bdf95e9417337d7df
purej1983 <purej1983@gmail.com>
2023-05-22 11:53:23 +0100

    refactor: loading and error handling (#8)
    * chore: fake dao for testing
    
    * feat: loading Indicator
    
    * feat: error handling
    
    * feat: adjust the http client timeout
    
    * chore: minior configuration change

--- app/src/main/java/com/thomaslam/chatgptclient/chatecompletion/presentation/Conversationscreen.kt
+++ app/src/main/java/com/thomaslam/chatgptclient/chatecompletion/presentation/Conversationscreen.kt
@@ -5,69 +5,99 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltNavGraphViewModel
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.thomaslam.chatgptclient.chatecompletion.presentation.components.AssitantMessageItem
 import com.thomaslam.chatgptclient.chatecompletion.presentation.components.MessageSendBar
 import com.thomaslam.chatgptclient.chatecompletion.presentation.components.UserMessageItem
-import com.thomaslam.chatgptclient.chatecompletion.presentation.util.Screen
 import com.thomaslam.chatgptclient.ui.theme.ChatGPTClientTheme
 import com.thomaslam.chatgptclient.ui.theme.assistantBackground
 import com.thomaslam.chatgptclient.ui.theme.userBackground
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun Conversationscreen(
-    navController: NavController,
     viewModel: ConversationViewModel = hiltViewModel()
 ) {
     val state by remember {
         viewModel.state
     }
 
-    var i = 0
-    Column(
-        modifier  = Modifier
-            .fillMaxHeight()
-            .background(MaterialTheme.colors.userBackground),
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
-        LazyColumn(modifier =
-        Modifier
-            .fillMaxWidth()
-            .weight(1f)
-        ) {
-            items(state.messages) { item ->
-                if (item.role == "user") {
-                    UserMessageItem(
-                        content = item.content,
-                        backgroundColor = MaterialTheme.colors.userBackground
-                    )
-                } else {
-                    AssitantMessageItem(
-                        content = item.content,
-                        backgroundColor = MaterialTheme.colors.assistantBackground
+    val scaffoldState = rememberScaffoldState()
+    val listState = rememberLazyListState()
+    LaunchedEffect(state.messages) {
+        listState.scrollToItem(state.messages.size)
+    }
+    LaunchedEffect(key1 = true) {
+        viewModel.eventFlow.collectLatest { event ->
+            when(event) {
+                is ConversationViewModel.UiEvent.ShowSnackBar -> {
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        message = event.message
                     )
                 }
-                i++
             }
         }
-        MessageSendBar(
-            modifier = Modifier
-                .background(MaterialTheme.colors.userBackground),
-            onMessageButtonClick = {
-                viewModel.send(it)
+    }
+
+    Scaffold(scaffoldState = scaffoldState)
+    {
+        Column(
+            modifier  = Modifier
+                .fillMaxHeight()
+                .background(MaterialTheme.colors.userBackground)
+                .padding(it),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            LazyColumn(modifier =
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)
+            ) {
+                items(state.messages) { item ->
+                    if (item.role == "user") {
+                        UserMessageItem(
+                            content = item.content,
+                            backgroundColor = MaterialTheme.colors.userBackground
+                        )
+                    } else {
+                        AssitantMessageItem(
+                            content = item.content,
+                            backgroundColor = MaterialTheme.colors.assistantBackground
+                        )
+                    }
+                }
             }
-        )
+            if(state.isLoading) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(0.dp, 8.dp)
+                )
+            }
+            MessageSendBar(
+                modifier = Modifier
+                    .background(MaterialTheme.colors.userBackground),
+                onMessageButtonClick = {
+                    viewModel.send(it)
+                }
+            )
+        }
     }
 }
 
@@ -75,6 +105,6 @@ fun Conversationscreen(
 @Composable
 private fun DefaultPreview() {
     ChatGPTClientTheme {
-        Conversationscreen(rememberNavController())
+        Conversationscreen()
     }
 }
---
+++ app/src/test/java/com/thomaslam/chatgptclient/chatecompletion/data/datasource/local/FakeChatGptDao.kt
@@ -0,0 +1,69 @@
+package com.thomaslam.chatgptclient.chatecompletion.data.datasource.local
+
+import com.thomaslam.chatgptclient.chatecompletion.data.datasource.ChatGptDao
+import com.thomaslam.chatgptclient.chatecompletion.data.local.entity.ChatEntity
+import com.thomaslam.chatgptclient.chatecompletion.data.local.entity.ConversationEntity
+import com.thomaslam.chatgptclient.chatecompletion.util.MockDataCollections
+
+class FakeChatGptDao: ChatGptDao {
+    private var chatAutoId: Long = 2
+    private var conversationAutoId: Long = 2
+
+    private val conversations = mockConversations
+
+    private val chats = mockChats
+    override suspend fun insertChat(chat: ChatEntity): Long {
+        chats.add(
+            ChatEntity(
+                lastUserMessage = chat.lastUserMessage,
+                id = ++chatAutoId
+            )
+        )
+        return chatAutoId
+    }
+
+    override suspend fun insertConversation(conversation: ConversationEntity) {
+        conversations.add(
+            ConversationEntity(
+                role = conversation.role,
+                content = conversation.content,
+                chatId = conversation.chatId,
+                id = ++conversationAutoId
+            )
+        )
+    }
+
+    override suspend fun updateLastUserMessage(chatId: Long, lastUserMessage: String) {
+        val index = chats.indexOfFirst { it.id == chatId }
+        chats[index] = ChatEntity(lastUserMessage, chatId)
+    }
+
+    override suspend fun getChats(): List<ChatEntity> {
+        return chats
+    }
+
+    override suspend fun getConversationByChatId(chatId: Long): List<ConversationEntity> {
+        return mockConversations
+    }
+    companion object {
+        val mockChats = mutableListOf(
+            ChatEntity("testMessage1", 1),
+            ChatEntity("testMessage2", 2),
+        )
+
+        val mockConversations = mutableListOf(
+            ConversationEntity(
+                id = 1,
+                role = MockDataCollections.userMessage1.role,
+                content = MockDataCollections.userMessage1.content,
+                chatId = 1
+            ),
+            ConversationEntity(
+                id = 2,
+                role = MockDataCollections.assistantMessage1.role,
+                content = MockDataCollections.assistantMessage1.content,
+                chatId = 1
+            )
+        )
+    }
+}
