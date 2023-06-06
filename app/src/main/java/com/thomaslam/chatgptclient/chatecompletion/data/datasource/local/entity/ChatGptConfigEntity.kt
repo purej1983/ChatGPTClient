a8752182690b5a8cdde0424024c7369278d81ea5
Thomas Lam <purej1983@gmail.com>
2023-06-06 16:14:15 +0100

    feat: new table for config

---
+++ app/src/main/java/com/thomaslam/chatgptclient/chatecompletion/data/datasource/local/entity/ChatGptConfigEntity.kt
@@ -0,0 +1,27 @@
+package com.thomaslam.chatgptclient.chatecompletion.data.datasource.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.thomaslam.chatgptclient.chatecompletion.domain.model.Config
+
+@Entity(
+    tableName = "Config",
+)
+data class ChatGptConfigEntity (
+    @PrimaryKey
+    val id: Short? = null,
+    val n: Int = 1,
+    val temperature: Float = 1f,
+    val stream: Boolean = false,
+    val max_tokens: Int = 150
+) {
+
+    fun toConfig(): Config {
+        return Config(
+            n = n,
+            temperature = temperature,
+            stream = stream,
+            max_tokens = max_tokens
+        )
+    }
+}
